6d8a4f50093e9936ca46c81b0e6d9829b9a0cd14
exarobotics-DJ <120158228+exarobotics-DJ@users.noreply.github.com>
2023-05-08 13:50:46 +0900

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
-basic.forever(function on_forever() {
+basic.forever(function () {
     basic.showLeds(`
         . # . # .
-                # . # . #
-                # . . . #
-                . # . # .
-                . . # . .
-    `)
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
     basic.pause(1000)
     basic.showLeds(`
         . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-    `)
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
     basic.pause(1000)
 })
